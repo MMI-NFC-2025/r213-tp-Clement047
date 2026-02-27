bd32d6e351a4826f850b76debda4753389fe4095
Clement047 <clement.bertrand02@edu.univ-fcomte.fr>
2026-02-27 11:53:53 +0100

    TP 5

--- src/backend.mjs
+++ src/backend.mjs
@@ -28,3 +28,34 @@ export async function getOffre(id) {
         return null;
     }
 }
+
+export async function addOffre(formData) {
+    try {
+        const record = await pb.collection("Maison").create(formData);
+        return {
+            success: true,
+            message: "Offre ajoutée avec succès"
+        };
+    } catch (error) {
+        console.log("Erreur ajout maison", error);
+        return {
+            success: false,
+            message: "Erreur lors de l'ajout"
+        };
+    }
+}
+
+export async function filterByPrix(minPrix, maxPrix) {
+    try {
+        // PocketBase filter (attention: champs = tes noms PB)
+        // Ici on suppose que ton champ s'appelle "prix"
+        const data = await pb.collection("Maison").getFullList({
+            sort: "-created",
+            filter: `prix >= ${minPrix} && prix <= ${maxPrix}`,
+        });
+        return data;
+    } catch (error) {
+        console.log("Une erreur est survenue en filtrant par prix", error);
+        return [];
+    }
+}
